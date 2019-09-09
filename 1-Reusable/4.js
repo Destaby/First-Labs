73823f7e3863831a0e92ec4fd1279da533edc010
Destaby <53400745+Destaby@users.noreply.github.com>
2019-09-09 23:52:49 +0300

    Create 4.js

---
+++ 1-Reusable/4.js
@@ -0,0 +1,27 @@
+'use script';
+
+let object1 = {
+    name: `Nick`
+}
+
+let object2 = {
+    name: `Nick`
+}
+
+object1.name = `Kate`;
+object2.name = `Rudyk`;
+
+console.log(object1.name, object2.name);
+
+// Ми дали полям name інші значення і вивели їх через console.log
+
+
+const createUser = (name, city) => {
+    const object = {
+        name: name,
+        city: city
+    }
+    console.log(object);
+}
+
+createUser(`Marcus Aurelius`, `Roma`);
