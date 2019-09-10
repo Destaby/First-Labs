335a9b7fe5ea4653d63f36b624b70b28920f45f0
Destaby <53400745+Destaby@users.noreply.github.com>
2019-09-10 11:43:10 +0300

    Create 3.js

---
+++ 2-DataTypes/3.js
@@ -0,0 +1,22 @@
+`use script`;
+
+const array = [true, 'hello', 5, 12, -200, false, false, 'word', null, undefined];
+let hash = {
+    number: 0,
+    string: 0,
+    boolean: 0,
+    object: 0,
+    undefined: 0
+};
+for (let x of array) {
+    hash[typeof x]++;
+}
+console.log(hash);
+
+const hash1 = {};
+
+for (let x of array) {
+    hash1[typeof x] = isNaN(hash1[typeof x]) ? 1 : hash1[typeof x] + 1;
+}
+console.log(hash1);
+
