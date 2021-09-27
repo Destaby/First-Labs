5a584abaa34a432a77049eb2baf736dc620f2dab
Destaby <rudik.nick.nick@gmail.com>
2021-09-27 17:17:21 +0300

    edit 4-5

--- 1-Reusable/4.js
+++ 1-Reusable/4.js
@@ -1,12 +1,12 @@
 'use script';
 
-let object1 = {  
-    name: `Nick`
-}
+let object1 = {
+  name: `Nick`,
+};
 
 let object2 = {
-    name: `Nick`
-}
+  name: `Nick`,
+};
 
 object1.name = `Kate`;
 object2.name = `Rudyk`;
@@ -15,13 +15,14 @@ console.log(object1.name, object2.name);
 
 // Ми дали полям name інші значення і вивели їх через console.log
 
-
 const createUser = (name, city) => {
-    const object = {
-        name: name,
-        city: city
-    }
-    console.log(object);
-}
+  const object = {
+    name: name,
+    city: city,
+  };
+  console.log(object);
+};
+
+// edit 4
 
 createUser(`Marcus Aurelius`, `Roma`);
