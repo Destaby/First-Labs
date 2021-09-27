badb2b941ddb630f8c86c517a8c74abf1f104f6a
Destaby <rudik.nick.nick@gmail.com>
2021-09-27 17:09:05 +0300

    6 edit

--- 1-Reusable/6.js
+++ 1-Reusable/6.js
@@ -1,18 +1,20 @@
 `use script`;
 
-let hash = { 
-    "Marcus Aurelius": "+380445554433",
-    "Doesn\`t matter": "+380982713276",
-    "Again": "+380999999999",
-    "That\`s boring": "+380000000000"
-}
+let hash = {
+  'Marcus Aurelius': '+380445554433',
+  'Doesn`t matter': '+380982713276',
+  Again: '+380999999999',
+  'That`s boring': '+380000000000',
+};
 
 let findPhoneByName = name => {
-    for (const x in hash) {
-        if (x == name) {
-            console.log(hash[x]);
-        }
+  for (const x in hash) {
+    if (x == name) {
+      console.log(hash[x]);
     }
-}
+  }
+};
 
 findPhoneByName(`Again`);
+
+// edit 6
