aed33bf73ca5fe5dcb2ebbfd2caaea42e2231c9b
Destaby <rudik.nick.nick@gmail.com>
2021-09-27 17:17:38 +0300

    edit 4-5-6

---
+++ 1-Reusable/5.js
@@ -0,0 +1,19 @@
+`use strict`;
+
+const array = [
+  { name: `Marcus Aurelius`, phone: `+380445554433` },
+  { name: `Doesn\`t matter`, phone: `+380982713276` },
+  { name: `Again`, phone: `+380999999999` },
+  { name: `That\`s boring`, phone: `+380000000000` },
+];
+
+let findPhoneByName = name => {
+  for (let i = 0; i < array.length; i++) {
+    if (name == array[i].name) {
+      console.log(array[i].phone);
+    }
+  }
+};
+findPhoneByName(`That\`s boring`);
+
+// edit 5
