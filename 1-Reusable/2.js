195c3e690fd3bf84cea4c49dadd6ff5fb60508c4
Destaby <rudik.nick.nick@gmail.com>
2021-09-27 16:54:56 +0300

    edit

--- 1-Reusable/2.js
+++ 1-Reusable/2.js
@@ -1,14 +1,17 @@
 `use script`;
 
-for (let i = 15; i < 30;i += 2) {  
-    console.log(i);
+for (let i = 15; i < 30; i += 2) {
+  console.log(i);
 }
 
 const range = (start, end) => {
-    for (start; start < end; start++) {
-        if (start % 2 == 1) {
-            console.log(start);
-        }
+  for (start; start < end; start++) {
+    if (start % 2 == 1) {
+      console.log(start);
     }
-}
+  }
+};
+
+// edit
+
 range(15, 30);
